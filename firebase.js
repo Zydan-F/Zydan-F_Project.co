cec7929a0fce16b555c2c331c99c05415e172337
Zydan-F <achizzanfathirazka@gmail.com>
2026-05-19 16:39:38 +0700

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,30 +1,23 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
-}
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAA5mzO0fzc0bMr9wnbBHm1f2nax4XrwQ4",
+    authDomain: "perpustakaan-digital-smakins.firebaseapp.com",
+    projectId: "perpustakaan-digital-smakins",
+    storageBucket: "perpustakaan-digital-smakins.firebasestorage.app",
+    messagingSenderId: "203203851480",
+    appId: "1:203203851480:web:8a0b63e9dce12819db53ad",
+    measurementId: "G-Y6N0ZJRM20"
+  };
 
-const firebaseConfig = {
-
-  apiKey: "API_KEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
-
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-
-export {
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
-};
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
